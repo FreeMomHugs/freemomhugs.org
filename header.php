396071476e6c89823e468863924f35696e70ffa9
Preston Kemp <preston@prestonkemp.com>
2021-05-07 00:35:45 -0500

    freshdesk widget

--- header.php
+++ header.php
@@ -27,6 +27,14 @@
     </script>
 
 
+    <script>
+        window.fwSettings={
+            'widget_id':43000001812
+        };
+        !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
+    </script>
+    <script type='text/javascript' src='https://widget.freshworks.com/widgets/43000001812.js' async defer></script>
+
 
 
 </head>
