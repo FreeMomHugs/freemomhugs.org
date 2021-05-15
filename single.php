c54889697298c3aeb7bf5488473b51785fe11cb2
Preston Kemp <preston@prestonkemp.com>
2021-05-14 19:43:03 -0500

    add opengraph data to wordpress

--- single.php
+++ single.php
@@ -11,6 +11,11 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+        <meta property="og:url"                content="<?php echo get_the_permalink(); ?>" />
+        <meta property="og:type"               content="article" />
+        <meta property="og:title"              content="<?php echo get_the_title(); ?>" />
+        <meta property="og:description"        content="<?php echo  get_the_excerpt(); ?>" />
+        <meta property="og:image"              content="<?php echo the_post_thumbnail_url(); ?>" />
 
 		<?php
 		while ( have_posts() ) :
