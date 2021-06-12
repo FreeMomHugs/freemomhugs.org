38d231505b3809f38fa6ee4ba3a279ec2d97b3f7
Preston Kemp <preston@prestonkemp.com>
2021-06-12 14:25:23 -0500

    pride-season home update

--- index.php
+++ index.php
@@ -49,42 +49,12 @@ get_header();
             </div> <!-- / .container -->
         </section>
 
-        <!-- ABOUT
-================================================== -->
-        <section class="pt-8 pb-8 pt-md-11 pb-md-11 bg-black">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-12 col-md-6 col-lg-6 align-items-center">
-                        <!-- Image -->
-                        <img class="mx-auto d-block" src="https://static.freemomhugs.org/wp-content/uploads/2021/05/pride-2021-shirt-small.png" alt="...">
-                    </div>
-                    <div class="col-12 col-md-6 col-lg-6 text-center">
-
-                        <!-- Heading -->
-                        <h2 class="display-3 font-weight-bold text-white mb-3">
-                            It's <span style="background: linear-gradient(to right, #fdf368, #fdad4b, #fb5173); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">Pride</span> Season.
-                        </h2>
-                        <p class="text-muted lead mb-0 pb-0">
-                            <span class="text-white">Official 2021 Pride Shirt</span>
-                        </p>
-
-                        <!-- Text -->
-                        <p class="text-muted lead mb-0 pb-0">
-                            <span class="text-white font-weight-bold">Available Now</span>
-                        </p>
-                        <p class="text-muted lead mb-6 pb-2">
-                            Order until June 7
-                        </p>
-
-                        <a class="btn btn-white btn-pill mb-6" href="https://oklahoma-shirt-company.printavo.com/merch/free-mom-hugs/products/pride-tee-21-135773">
-                            Order Now
-                        </a>
-
-                    </div>
-
-                </div> <!-- / .row -->
-            </div>
-        </section>
+        <?php
+        $date_now = date("Y-m-d"); // this format is string comparable
+        if ($date_now < '2021-06-12') {
+            get_template_part( 'template-parts/pride-season');
+        }
+        ?>
 
 
         <?php
