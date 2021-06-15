10e67579b8b64524647f28a06c2809ae6a622b55
Preston Kemp <preston@prestonkemp.com>
2021-06-15 15:43:15 -0500

    Auto stash before merge of "main" and "origin/main"

--- searchpage.php
+++ searchpage.php
@@ -1,4 +1,10 @@
 <?php
+
+/**
+ * Template Name: Search Page
+ */
+
+
 /**
  * The template for displaying all pages
  *
@@ -14,25 +20,91 @@
 
 get_header();
 ?>
+    <!-- BREADCRUMB
+    ================================================== -->
+    <nav class="bg-gray-200">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
 
-	<main id="primary" class="site-main">
+                    <!-- Breadcrumb -->
+                    <ol class="breadcrumb breadcrumb-scroll">
+                        <li class="breadcrumb-item">
+                            <a href="blog.html" class="text-gray-700">
+                                Blog
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            Search Results
+                        </li>
+                    </ol>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+                </div>
+            </div> <!-- / .row -->
+        </div> <!-- / .container -->
+    </nav>
 
-			get_template_part( 'template-parts/content', 'page' );
+    <!-- SEARCH
+    ================================================== -->
+    <section class="py-6">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+                    <!-- Form -->
+                    <?php get_search_form(); ?>
+                    <form class="rounded shadow">
+                        <div class="input-group input-group-lg">
 
-		endwhile; // End of the loop.
-		?>
+                            <!-- Prepend -->
+                            <div class="input-group-prepend">
+                  <span class="input-group-text border-0 pr-1">
+                    <i class="fe fe-search"></i>
+                  </span>
+                            </div>
 
-	</main><!-- #main -->
+                            <!-- Input -->
+                            <input type="text" class="form-control border-0 px-1" aria-label="Search our blog..." placeholder="Search our blog..." value="<?php echo get_search_query(); ?>">
+
+                            <!-- Append -->
+                            <div class="input-group-append">
+                  <span class="input-group-text border-0 py-0 pl-1 pr-3">
+
+                    <!-- Text -->
+                      <?php
+                      global $wp_query;
+                      $total_results = $wp_query->found_posts;
+                      ?>
+                    <span class="h6 text-uppercase text-muted d-none d-md-block mb-0 mr-5">
+                      <?php echo $total_results;?> results
+                    </span>
+
+                      <!-- Button -->
+                    <button type="submit" class="btn btn-sm btn-primary" value="Search">
+                      Search
+                    </button>
+
+                  </span>
+                            </div>
+
+                        </div>
+                    </form>
+
+                </div>
+            </div> <!-- / .row -->
+        </div>
+    </section>
+    <main id="primary" class="site-main">
+
+    <?php
+    get_template_part( 'template-parts/newsroom-first-post', 'page' );
+    get_template_part( 'template-parts/newsroom-cards', 'page' );
+    ?>
+
+
+    </main><!-- #main -->
 
 <?php
-get_sidebar();
+get_template_part( 'template-parts/shape', 'none');
+get_template_part( 'template-parts/cta-newsletter-sub', 'none');
 get_footer();
