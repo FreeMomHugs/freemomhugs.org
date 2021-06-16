3e87dd95a4d8460643037dfaf9954a7d1d994322
Preston Kemp <preston@prestonkemp.com>
2021-06-16 09:49:13 -0500

    add Google analytics

--- header.php
+++ header.php
@@ -41,6 +41,17 @@
     }
     ?>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164236741-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-164236741-2');
+    </script>
+
+
 </head>
 
 <body <?php body_class(); ?>>
