70e9120b8a364fda369e058fbf6b09af87ef6f87
Preston Kemp <preston@prestonkemp.com>
2021-07-23 18:45:32 -0500

    finish pray-away section for doc

--- template-parts/pray-away-section.php
+++ template-parts/pray-away-section.php
@@ -8,34 +8,44 @@
             <div class="col-12 col-md-8 col-lg-9 mt-8 text-center">
 
                 <!-- Heading -->
+                <div class="row">
+                    <!-- Image for awards PA -->
+                    <div class="col-12 col-md-6 mx-auto text-center pl-6 pr-6 pb-8">
+                        <img class="mx-auto d-block" src="https://static.freemomhugs.org/wp-content/uploads/2021/07/PA-Netflix-announcement-no-title.png" alt="...">
+                    </div>
+                </div>
                 <h1 class="display-1 font-weight-bold text-white">
                     Pray Away</span>
                 </h1>
 
                 <p class="text-muted lead mb-4 pb-8">
-                    <span class="text-white">Former leaders of the "pray the gay away" movement contend with the aftermath unleashed by their actions, while a survivor seeks healing and acceptance from more than a decade of trauma.</span>
+                    <span class="text-white font-italic">Former leaders of the "pray the gay away" movement contend with the aftermath unleashed by their actions, while a survivor seeks healing and acceptance from more than a decade of trauma.</span>
                 </p>
+            </div>
 
 
-
-                <div class="row">
-                    <div class="col-6 text-center">
+            <div class="container">
+                <div class="row mb-8">
+                    <div class="col-12 col-md-6 text-center align-self-center">
 
                         <!-- Heading -->
                         <h3 class="font-weight-bold text-white mb-3">
                             Panel Discussion
                         </h3>
                         <p class="text-muted mb-2 pb-0">
-                            <span class="text-white">Hear from survivors of conversion therapy</span>
+                            <span class="text-white">Free Mom Hugs hosts a panel discussion with conversion therapy survivors</span>
+                        </p>
+                        <p class="text-muted font-weight-bold mb-2 pb-0">
+                            <span class="text-white">August 4 @ 7pm CT</span>
                         </p>
 
                         <!-- Text -->
-                        <a class="btn btn-white btn-pill mb-6" href="https://oklahoma-shirt-company.printavo.com/merch/free-mom-hugs/products/pride-tee-21-135773">
-                            Watch on YouTube
+                        <a class="btn btn-danger btn-pill text-white disabled" href="">
+                            Coming Soon
                         </a>
-
                     </div>
-                    <div class="col-6">
+
+                    <div class="col-12 col-md-6 text-center align-self-center">
 
                         <!-- Heading -->
                         <h3 class="font-weight-bold text-white mb-3">
@@ -46,36 +56,34 @@
                         </p>
 
                         <!-- Text -->
-                        <a class="btn btn-white btn-pill mb-6" href="https://oklahoma-shirt-company.printavo.com/merch/free-mom-hugs/products/pride-tee-21-135773">
+                        <a class="btn btn-danger btn-pill text-white" href="https://about.netflix.com/en/news/documentary-film-pray-away-heads-to-netflix">
                             Learn more on Netflix
                         </a>
 
                     </div>
                 </div>
             </div>
-
-            <div class="col-6 mb-8">
-                <div class="text-center">
-                    <p class="font-weight-bold text-white">
-                        Watch the Trailer
-                    </p>
-                </div>
-                <div class="row">
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube-nocookie.com/embed/tk_CqGVfxEs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
+            <!-- Trailer -->
+            <div class="col-12 col-md-6 mb-8">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube-nocookie.com/embed/tk_CqGVfxEs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
+
+
 
 
             <div class="row">
                 <!-- Image for awards PA -->
-                <div class="col-6 mx-auto text-center">
-                    <img class="mx-auto d-block" src="https://static.freemomhugs.org/wp-content/uploads/2021/07/PA-Netflix-announcement-no-title.png" alt="...">
+                <div class="col-12 col-md-6 mx-auto text-center pl-6 pr-6">
                     <img class="mx-auto d-block" src="https://static.freemomhugs.org/wp-content/uploads/2021/07/PAlaurelchain.png" alt="...">
 
                 </div>
             </div>
+
+
+
+
 
         </div> <!-- / .row -->
     </div> <!-- / .container -->
