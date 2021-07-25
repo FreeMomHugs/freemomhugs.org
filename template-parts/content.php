66fede490b1cfab49c0d38754576cd50123fdbc1
Preston Kemp <preston@prestonkemp.com>
2021-07-24 19:18:47 -0500

    fix spacing issues for header and excerpt

--- template-parts/content.php
+++ template-parts/content.php
@@ -39,7 +39,7 @@
 
 
                 <div class="row">
-                    <div class="col-12 col-md-10 col-lg-9 col-xl-8">
+                    <div class="col-12">
 
                         <!-- Heading -->
 
