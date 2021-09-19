e04150375482c2111639af6c70f225755d23ae9d
Preston Kemp <preston@prestonkemp.com>
2021-09-18 23:15:25 -0500

    updates to resource pages and css changes

--- page-books.php
+++ page-books.php
@@ -19,24 +19,9 @@ get_header();
 
         <!-- WELCOME
          ================================================== -->
-        <div class="row bg-dark">
-            <div class="container">
-                <div class="col-12 ml-0 pl-0">
-                    <nav aria-label="breadcrumb" >
-                        <ol class="breadcrumb breadcrumb-scroll">
-                            <li class="breadcrumb-item">
-                                <a class="text-white font-size-lg" href="#" style="font-weight: 600;">
-                                    Resources
-                                </a>
-                            </li>
-                            <li class="breadcrumb-item active text-white-70 font-size-lg" aria-current="page" style="font-weight: 600;">
-	                            <?php echo get_the_title(); ?>
-                            </li>
-                        </ol>
-                    </nav>
-                </div>
-            </div>
-        </div>
+            <?php
+                get_template_part( 'template-parts/resources-breadcrumb', 'page' );
+            ?>
             <div class="container">
                 <div class="row text-left">
                     <div class="col-12">
---
+++ template-parts/resources-breadcrumb.php
@@ -0,0 +1,18 @@
+<div class="row bg-dark">
+    <div class="container">
+        <div class="col-12 ml-4 ml-lg-0 pl-0">
+            <nav aria-label="breadcrumb" >
+                <ol class="breadcrumb breadcrumb-scroll">
+                    <li class="breadcrumb-item">
+                        <a class="text-white font-size-lg" href="/resources" style="font-weight: 600;">
+                            Resources
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item active text-white-70 font-size-lg" aria-current="page" style="font-weight: 600;">
+						<?php echo get_the_title(); ?>
+                    </li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
