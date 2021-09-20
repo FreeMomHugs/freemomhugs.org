86d34497f02ce1b463a561448005016b1b5d2554
Preston Kemp <preston@prestonkemp.com>
2021-09-19 19:24:15 -0500

    update mission statement template and css changes

--- template-parts/mission-statement.php
+++ template-parts/mission-statement.php
@@ -3,22 +3,17 @@
 <a class="d-block text-decoration-none" id="sectionTwo" data-jarallax data-speed=".8" href="" style="background-image: url(<?php echo $args['background-url']; ?>)">
     <div class="container d-flex flex-column">
         <div class="row align-items-center justify-content-center min-vh-100 py-8 py-md-11">
-            <div class="col-12 text-center">
-
+            <div class="col-12 text-center px-2">
                 <!-- Subheading -->
                 <h4 class="mb-0 font-weight-bold text-center text-uppercase text-dark letter-spacing-lg">
                     Our Mission
                 </h4>
-
                 <h3 class="display-3 text-black">
-                    Empowering the world to celebrate the
-                    LGBTQIA+ community through visibility,
-                    education, and conversation.
-                        <br>
-                    <span class="text-primary font-weight-bold" style="background: linear-gradient(to right, #70d6ff, #ff70a6, #ff9770); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">And yes, we still give hugs!</span>
+                    Empowering the world to celebrate the LGBTQIA+ community through visibility, education, and conversation.
                 </h3>
-
-
+                <h3 class="display-3 font-weight-bold" style="background: linear-gradient(to right, #70d6ff, #ff70a6, #ff9770); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">
+                    And yes, we still give hugs!
+                </h3>
             </div>
         </div>
     </div>
