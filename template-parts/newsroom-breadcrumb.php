d470eed2fcaecc693a2eeb3b3eb12a1d06d4d44e
Preston Kemp <preston@prestonkemp.com>
2021-09-22 15:18:51 -0500

    fix search on mobile

--- template-parts/newsroom-breadcrumb.php
+++ template-parts/newsroom-breadcrumb.php
@@ -1,20 +1,21 @@
 
 <div class="bg-gray-200">
     <div class="container">
-        <nav class="navbar navbar-expand-lg" aria-label="navbar">
-            <ul class="navbar-nav mr-auto">
-                <li class="breadcrumb-item">
-                    <a class="text-dark font-size-lg" href="/newsroom" style="font-weight: 600;">
-                        Newsroom
-                    </a>
-                </li>
-            </ul>
-            <form class="form-inline my-0" action="/" method="GET">
-                <input name="s" type="text" class="py-2 h-100 mr-sm-2 form-control border-0" aria-label="Search" placeholder="Search">
-                <button type="submit" class="btn btn-sm btn-outline-black py-1 h-100">
-                    Search
-                </button>
-            </form>
+        <nav class="navbar navbar-expand-lg sticky-top" aria-label="navbar" id="newsroomNavbar">
+            <a class=" navbar-brand text-dark font-size-lg" href="/newsroom" style="font-weight: 600;">
+                Newsroom
+            </a>
+            <button class="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#newsroomNavbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="newsroomNavbarCollapse" style="position:relative;left: 0; top: 1rem;">
+                <form class="form-inline my-4 my-md-0" action="/" method="GET">
+                    <input name="s" type="text" class="py-2 h-100 mr-sm-2 form-control border-0 border-sm-1" aria-label="Search" placeholder="Search">
+                    <button type="submit" class="btn btn-sm btn-outline-black py-1 h-100 mt-2 mt-md-0 ml-3">
+                        Search
+                    </button>
+                </form>
+            </div>
         </nav>
     </div>
 </div>
