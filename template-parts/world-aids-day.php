a10ae6cb62d3c747ebe5bcd83ae82200530851b5
Preston Kemp <preston@prestonkemp.com>
2021-12-19 10:39:39 -0600

    world AIDS day stuff

---
+++ template-parts/world-aids-day.php
@@ -0,0 +1,32 @@
+<!-- pride season
+
+================================================== -->
+
+<section class="bg-cover" style="background-color: #3f0d12; background-image: linear-gradient(180deg, #3f0d12 0%, #a71d31 74%);">
+    <div class="container d-flex flex-column text-white">
+        <div class="row align-items-center justify-content-center min-vh-100 py-8 py-md-11">
+            <div class="col-12 col-md-8 col-lg-9 mt-8 text-center">
+
+                <!-- Heading -->
+                <div class="row">
+                    <div class="col-4 mx-auto text-center pl-6 pr-6 pb-3">
+                        <img class="mx-auto d-block" src="https://static.freemomhugs.org/images/site/world-aids-day/red-ribbon.svg" alt="...">
+                    </div>
+                    <div class="col-12 mx-auto text-center pl-6 pr-6 pb-8">
+                        <h1 class="display-1 font-weight-bold text-white">World AIDS Day</h1>
+                        <h4 class="display-4 font-weight-bold">December 1</h4>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+        </div> <!-- / .row -->
+    </div> <!-- / .container -->
+</section>
+<?php
